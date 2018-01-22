6f2d2a703db45299816c3e010d3c64e2f1f8d5fd
BetoProgram <juancho34_8@gmail.com>
2018-01-21 23:02:04 -0600

    se implementa plantilla ys ecrean primeros componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,28 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { BreadcrumsComponent } from './shared/breadcrums/breadcrums.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    LoginComponent,
+    NopagefoundComponent,
+    DashboardComponent,
+    Graficas1Component,
+    ProgressComponent,
+    BreadcrumsComponent,
+    HeaderComponent,
+    SidebarComponent
+],
   imports: [
     BrowserModule
   ],
